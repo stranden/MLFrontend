25de9062604ff34a342f538db613cad87afc2dbd
stranden <henrik@hens.dk>
2024-02-17 10:32:57 +0100

    Minor fix

--- src/components/Target.vue
+++ src/components/Target.vue
@@ -50,7 +50,7 @@ export default {
             // Draw target content using your createTarget function
             createTarget(this.targetName, targetSVG, targetContainerWidth, this.shotData);
 
-            if (this.flags !== ''){
+            if (this.flags !== '' || this.flags !== 'W'){
                 // Create circle element for overlay
                 const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
                 circle.setAttribute("cx", "50%");
