f82fe30358685e8c59340b2ea817cbd2454323d2
stranden <henrik@hens.dk>
2024-02-17 10:31:11 +0100

    Added W as flags

--- src/components/ShooterDisplay.vue
+++ src/components/ShooterDisplay.vue
@@ -118,7 +118,7 @@ export default {
             return array.filter(item => item[key] === value);
         },
         includeAllShooters(array) {
-            return array.filter(item => item['flags'] === '' || item['flags'] === 'E' || item['flags'] === 'P');
+            return array.filter(item => item['flags'] === '' || item['flags'] === 'E' || item['flags'] === 'P' || item['flags'] === 'W');
         },
         svgSource(country) {
             if (country) {
