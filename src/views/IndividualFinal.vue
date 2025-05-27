71c09fa12a605c35562e074edb2001e6c7f7a850
stranden <henrik@hens.dk>
2025-05-27 23:21:55 +0200

    Started on series logic

--- src/views/IndividualFinal.vue
+++ src/views/IndividualFinal.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="stageInfo.stage === 'series' || stageInfo.stage === 'presentation'">        
+    <div v-if="stageInfo.fiveShotsSeries">        
         <div id="shootingDisplayContainer">
             <div v-for="(data, index) in activeShooters" :key="index" :class="getShooterClass(data.flags)">
                 <Target :targetName="data.targetId" :shotData="extractShotsForShooter(data)" :flags="data.flags" />
@@ -20,6 +20,28 @@
                 </div>
                 <div class="clubTextContainer">
                     <div class="clubText"><img :src="svgSource(parseClubData(data.club).nation)" alt="nation" /><span>{{ parseClubData(data.club).club }}</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div v-else-if="stageInfo.seriesTypeShootoff">        
+        <div id="shootingDisplayContainer">
+            <div v-for="(data, index) in activeShooters" :key="index" :class="getShooterClass(data.flags)">
+                <Target :targetName="data.targetId" :shotData="extractShotsForShooter(data)" :flags="data.flags" />
+                <div class="scoreTextTopRightContainer">
+                    <div class="scoreTextTopRight">SCORE</div>
+                </div>
+                <div class="scoreShotValueContainer">
+                    <div class="scoreShotValue">{{ data.shots.length > 0 ? data.shots[data.shots.length - 1].vd : '0.0' }}</div>
+                </div>
+                <div class="scoreTotalContainer">
+                    <div class="scoreTotal">{{ data.totalScore }}</div>
+                </div>
+                <div class="scoreTotalTextContainer">
+                    <div class="scoreTotalText">Total</div>
+                </div>
+                <div class="nameTextContainer">
+                    <div class="nameText">{{ formatName(data.name) }}</div>
                 </div>
             </div>
         </div>
@@ -60,40 +82,43 @@ export default {
             // Check for specific flags in the fetched data
             const hasShootOff = this.fetchedData.some(s => s.flags === 'T');
             const hasPendingElimination = this.fetchedData.some(s => ['P', 'SP'].includes(s.flags));
-            
+            const seriesTypeShootoff = this.fetchedData.some(s => s.seriesType === 'shootoff');
+
             // Determine the stage based on the number of shots
             const fiveShotsSeries = Math.max(...this.fetchedData.map(p => p.matchShotCount)) <= 10;
             
             // Check the number of eliminations
-            const firstElimination = active === 7;
-            const secondElimination = active === 6;
-            const thirdElimination = active === 5;
-            const fourthElimination = active === 4;
-            const fifthElimination = active === 3;
-            const sixthElimination = active === 2;
-            const finalElimination = active === 1;
+            const firstSingleShotSeries = active === 8 && !fiveShotsSeries;
+            const secondSingleShotSeries = active === 7 && !seriesTypeShootoff;
+            const thirdSingleShotSeries = active === 6 && !seriesTypeShootoff;
+            const fourthSingleShotSeries = active === 5 && !seriesTypeShootoff;
+            const fifthSingleShotSeries = active === 4 && !seriesTypeShootoff;
+            const sixthSingleShotSeries = active === 3 && !seriesTypeShootoff;
+            const seventhSingleShotSeries = active === 2 && !seriesTypeShootoff;
 
             // Return the stage information
             return {
                 fiveShotsSeries,
-                firstElimination,
-                secondElimination,
-                thirdElimination,
-                fourthElimination,
-                fifthElimination,
-                sixthElimination,
-                finalElimination,
+                firstSingleShotSeries,
+                secondSingleShotSeries,
+                thirdSingleShotSeries,
+                fourthSingleShotSeries,
+                fifthSingleShotSeries,
+                sixthSingleShotSeries,
+                seventhSingleShotSeries,
                 hasShootOff,
+                seriesTypeShootoff,
                 hasPendingElimination,
                 stage: fiveShotsSeries ? 'series'
-                    : firstElimination ? 'first-elimination'
-                    : secondElimination ? 'second-elimination'
-                    : thirdElimination ? 'third-elimination'
-                    : fourthElimination ? 'fourth-elimination'
-                    : fifthElimination ? 'fifth-elimination'
-                    : sixthElimination ? 'sixth-elimination'
-                    : finalElimination ? 'final-elimination'
+                    : firstSingleShotSeries ? 'first-single-shot-series'
+                    : secondSingleShotSeries ? 'second-single-shot-series'
+                    : thirdSingleShotSeries ? 'third-single-shot-series'
+                    : fourthSingleShotSeries ? 'fourth-single-shot-series'
+                    : fifthSingleShotSeries ? 'fifth-single-shot-series'
+                    : sixthSingleShotSeries ? 'sixth-single-shot-series'
+                    : seventhSingleShotSeries ? 'final-single-shot-series'
                     : hasShootOff ? 'shootoff'
+                    : seriesTypeShootoff ? 'series-type-shootoff'
                     : hasPendingElimination ? 'presentation'
                     : 'regular'
             };
